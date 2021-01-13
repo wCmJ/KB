6dec8091c10f9f171d5579a0102f96f6218dd8c1
youliwei <youliwei@kuaishou.com>
2021-01-13 13:42:02 +0800

    remove test/googleTestExample

--- test/googleTestExample/build/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/youliwei/Documents/github/KS-boost.asio/test/googleTestExample/build/googletest-src/googletest
-# Build directory: /Users/youliwei/Documents/github/KS-boost.asio/test/googleTestExample/build/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
